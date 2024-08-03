7f101a1b7ccaad35195b6bc8b0c913bb72056f62
Kvashnina Olga <kalyaginaoa@gmail.com>
2024-08-03 16:29:10 +0500

    Версия 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list");
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 const createCard = (card, deleteCard) => {
   const cardElement = cardTemplate
-    .querySelector(".places__item")
+    .querySelector('.places__item')
     .cloneNode(true);
-  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
-  const cardImage = cardElement.querySelector(".card__image");
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
   cardImage.src = card.link;
   cardImage.alt = card.name;
-  cardElement.querySelector(".card__title").textContent = card.name;
-  cardDeleteButton.addEventListener("click", deleteCard);
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardDeleteButton.addEventListener('click', deleteCard);
   return cardElement;
 };
 
